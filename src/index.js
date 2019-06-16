bc007c67b8396a28c2a702d5ed4ad2f8e910ad8c
Jaid <jaid.jsx@gmail.com>
2019-06-16 13:22:19 +0200

    Added function `unpackObject`
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- src/index.js
+++ src/index.js
@@ -32,11 +32,12 @@ export const ensureArray = value => {
 /**
  * Returns `value`, or `{key: value}` if `value` is not an object
  * @param {*} value The value that will be converted to an array
+ * @param {string} key The key of the object entry that might be created on a new object
  * @example
  * import {ensureObject} from "magina"
  * ensureObject("abc", "importantKey")
  * // { importantKey: "abc" }
- * @returns {object} An array
+ * @returns {object} An object
  */
 export const ensureObject = (value, key) => {
   if (value === undefined) {
@@ -51,4 +52,20 @@ export const ensureObject = (value, key) => {
   }
 }
 
-export default ensureObject
+/**
+ * Returns `value`, or `value[key]` if `value` is not an object
+ * @param {*} value The value that might be an object
+ * @param {string} key The key whose value gets used on possible unpacking
+ * @example
+ * import {unpackObject} from "magina"
+ * unpackObject({id: "abc"}, "id")
+ * // "abc"
+ * @returns {object} Either the value or an unpacked value
+ */
+export const unpackObject = (value, key) => {
+  if (value |> isObject) {
+    return value[key]
+  } else {
+    return value
+  }
+}
--- test/test.js
+++ test/test.js
@@ -1,16 +1,18 @@
 import path from "path"
 
 const indexModule = (process.env.MAIN ? path.resolve(process.env.MAIN) : path.join(__dirname, "..", "src")) |> require
-const {ensureArray, ensureObject} = indexModule
+const {ensureArray, ensureObject, unpackObject} = indexModule
 
 it("should run", () => {
   expect(ensureArray("abc")).toStrictEqual(["abc"])
   expect(ensureObject(300, "speed")).toStrictEqual({speed: 300})
+  expect(unpackObject({id: "abc"}, "id")).toStrictEqual("abc")
 })
 
 it("should run without changes", () => {
   expect(ensureArray(["abc"])).toStrictEqual(["abc"])
   expect(ensureObject({speed: 300}, "speed")).toStrictEqual({speed: 300})
+  expect(unpackObject("abc", "id")).toStrictEqual("abc")
 })
 
 
